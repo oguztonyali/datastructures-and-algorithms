d3a936d735efb4b86985382539870e9b07e44d4b
oguztonyali <114167582+oguztonyali@users.noreply.github.com>
2023-04-19 10:10:23 +0300

    LinkedList Remove Duplicates
    LinkedList Remove Duplicates in Python

---
+++ LinkedList/LL-Remove_Duplicates.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next          
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    # WRITE REMOVE_DUPLICATES METHOD HERE #
+    def remove_duplicates(self):
+        values = set()
+        previous = None
+        current = self.head
+        while current:
+            if current.value in values:
+                previous.next = current.next
+                current = previous.next
+                self.length -= 1
+            else:
+                values.add(current.value)
+                previous = current
+                current = current.next
+                
+        
+    #######################################
+    
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(2)
+my_linked_list.append(4)
+my_linked_list.remove_duplicates()
+
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1
+    2
+    3
+    4
+    
+"""
